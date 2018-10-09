7338db16cefd0b6e197959d09ee66525572d4778
Steven Skoczen <steven@quantumimagery.com>
2018-10-09 16:45:44 +1300

    Adds memcached repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -574,6 +574,7 @@ start_setup() {
     setup_codebase firebase-backend
     setup_codebase dewey
     setup_codebase marketing
+    setup_codebase memcached
     setup_codebase hook
     setup_codebase meta
     setup_codebase portal
